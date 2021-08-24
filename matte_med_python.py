86cbca7c580f271fce1d9eb3620748d4b3ca1791
wangkking <wangkingson@gmail.com>
2021-08-24 13:16:38 +0200

    lagt til matte

---
+++ matte_med_python.py
@@ -0,0 +1,10 @@
+
+høyden=str(input("Skriv høyden: "))
+bredden=str(input("skriv bredden: "))
+
+areal = høyden*bredden
+omkrets= høyden*2+bredden+2
+
+print("Areale er på objekte: " + str(areal))
+print("Omkrestsen til objekte er: " + str(omkrets))
+
