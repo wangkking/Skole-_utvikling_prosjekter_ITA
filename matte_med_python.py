c00b245adcdfb8a593ffa4100644217f7989ba24
wangkking <wangkingson@gmail.com>
2021-08-24 13:27:06 +0200

    lagt til en areal av trekant

--- matte_med_python.py
+++ matte_med_python.py
@@ -1,10 +1,18 @@
 
-høyden=(input("Skriv høyden: "))
-bredden=(input("skriv bredden: "))
+høyden=int(input("Skriv høyden: "))
+bredden=int(input("skriv bredden: ")+ "\n")
 
 areal = høyden*bredden
 omkrets= høyden*2+bredden+2
 
 print("Areale er på objekte: " + str(areal))
-print("Omkrestsen til objekte er: " + str(omkrets))
+print("Omkrestsen til objekte er: " + str(omkrets)+"\n")
 
+#regne ut areal en trekant
+
+grunlinje = int(input("Grunlinje: "))
+høydde = int(input("Høydde: ")+"\n")
+
+areal = høydde*grunlinje/2
+
+print ("Arealet til trekanten var: " + str(areal))
